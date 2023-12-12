d363efcc26e43e8020e95ad4ff85d21ced695c8f
faithruth <faithruth27@gmail.com>
2023-12-12 20:43:36 +0300

    Create component

--- blockli-custom-components/Moviecard1.js
+++ blockli-custom-components/Moviecard1.js
@@ -2,31 +2,31 @@
 // Contributors: Cody Lamens, Philip Ikpeiwunna, Dayo Akins, Faith Ruth, Emmanuel Ademola, Nia Ikpeiwunna 
 
 // Importing necessary components from React and React Native
-import React from &#039;react&#039;;
-import { View, Text, Image, StyleSheet } from &#039;react-native&#039;;
+import React from &amp;#039;react&amp;#039;;
+import { View, Text, Image, StyleSheet } from &amp;#039;react-native&amp;#039;;
 
 // Component: MovieCard1 
 // This component is a functional component that displays a MovieCard1 
-const MovieCard1 = ({  }) =&gt; {
-  // Component&#039;s return statement with JSX 
+const MovieCard1 = ({  }) =&amp;gt; {
+  // Component&amp;#039;s return statement with JSX 
   return (
-    &lt;View style={styles.textContainer}&gt;
-      &lt;Text style={styles.title}&gt;Spiderman: Far From Home&lt;/Text&gt;
-      &lt;Text style={styles.subtitle}&gt;Action/Adventure • 2h 9m&lt;/Text&gt;
-      &lt;View style={styles.ratingContainer}&gt;
-        &lt;Text style={styles.rating}&gt;⭐ 5.4 Stars&lt;/Text&gt;
-      &lt;/View&gt;
-    &lt;/View&gt;
+    &amp;lt;View style={styles.textContainer}&amp;gt;
+      &amp;lt;Text style={styles.title}&amp;gt;Spiderman: Far From Home&amp;lt;/Text&amp;gt;
+      &amp;lt;Text style={styles.subtitle}&amp;gt;Action/Adventure • 2h 9m&amp;lt;/Text&amp;gt;
+      &amp;lt;View style={styles.ratingContainer}&amp;gt;
+        &amp;lt;Text style={styles.rating}&amp;gt;⭐ 5.4 Stars&amp;lt;/Text&amp;gt;
+      &amp;lt;/View&amp;gt;
+    &amp;lt;/View&amp;gt;
   );
 };
 
 // StyleSheet for styling the components 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: &#039;#fff&#039;,
+    backgroundColor: &amp;#039;#fff&amp;#039;,
     borderRadius: 8,
-    overflow: &#039;hidden&#039;,
-    shadowColor: &#039;#000&#039;,
+    overflow: &amp;#039;hidden&amp;#039;,
+    shadowColor: &amp;#039;#000&amp;#039;,
     shadowOffset: {
       width: 0,
       height: 2,
@@ -35,35 +35,35 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
     /* Additional properties */
-    test: &#039;test&#039;,
+    test: &amp;#039;test&amp;#039;,
   },
   image: {
-    width: &#039;100%&#039;,
+    width: &amp;#039;100%&amp;#039;,
     height: 200,
     // Set this value to maintain aspect ratio of the image
-    resizeMode: &#039;cover&#039;,
+    resizeMode: &amp;#039;cover&amp;#039;,
   },
   textContainer: {
     padding: 16,
   },
   title: {
     fontSize: 20,
-    fontWeight: &#039;bold&#039;,
+    fontWeight: &amp;#039;bold&amp;#039;,
     marginBottom: 4,
   },
   subtitle: {
     fontSize: 14,
-    color: &#039;#666&#039;,
+    color: &amp;#039;#666&amp;#039;,
     marginBottom: 8,
   },
   ratingContainer: {
-    flexDirection: &#039;row&#039;,
-    alignItems: &#039;center&#039;,
+    flexDirection: &amp;#039;row&amp;#039;,
+    alignItems: &amp;#039;center&amp;#039;,
   },
   rating: {
     fontSize: 14,
-    color: &#039;#ffc107&#039;,
-    // You can adjust the color to match the star&#039;s color
+    color: &amp;#039;#ffc107&amp;#039;,
+    // You can adjust the color to match the star&amp;#039;s color
   },
 });
 
